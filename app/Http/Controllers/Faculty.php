3793664db17f48b6655538de93c9771f145909d3
rrrrynnnns <harry.banas@urios.edu.ph>
2025-09-05 11:25:56 +0800

    act4

---
+++ app/Http/Controllers/Faculty.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\c;
+use Illuminate\Http\Request;
+
+class Faculty extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\c  $c
+     * @return \Illuminate\Http\Response
+     */
+    public function show(c $c)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\c  $c
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, c $c)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\c  $c
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(c $c)
+    {
+        //
+    }
+}
